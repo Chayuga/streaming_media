e2d0abe1b2d9bfd808c8f53049f01fe8ef9fb816
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-31 23:39:30 +0300

    feat: sidebar implementation

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { Sidebar } from '.';
+
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
@@ -11,7 +13,7 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        Sidebar
+        <Sidebar />
         <Typography
           className='copyright'
           variant='body2'
